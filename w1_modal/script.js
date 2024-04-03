921c46fb16e8aa32fd0b1057484d3bde99380944
dohyung01 <154952683+dohyung01@users.noreply.github.com>
2024-04-04 01:13:49 +0900

    last2

--- w1_modal/script.js
+++
@@ -1,7 +0,0 @@
-
-const open_btn = document.querySelector("#open_btn");
-const close_btn = document.querySelector("#close_btn");
-const modal_wrapper = document.querySelector(".modal-wrapper");
-
-open_btn.addEventListener('click', () => { modal_wrapper.style.display = 'flex' });
-close_btn.addEventListener('click', () => { modal_wrapper.style.display = 'none' });
